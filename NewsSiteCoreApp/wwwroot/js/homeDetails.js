9dddacc5890324511ac752458fefc23cdccf6f74
anarfarzaliyev9 <anarfarzaliyev9@gmail.com>
2020-08-06 23:48:03 +0400

    Bug fixes

---
+++ NewsSiteCoreApp/wwwroot/js/homeDetails.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+
+    var headerContainer = $(".header-container");
+    var sticky = $(headerContainer).offset().top;
+    var leftInfo = $(".left-info");
+    var sticky2 = $(leftInfo).offset().top;
+    $(window).scroll(function () {
+
+        if ($(window).scrollTop() > sticky) {
+            $(headerContainer).addClass("sticky")
+
+        } else if ($(window).scrollTop() == sticky) {
+            $(headerContainer).removeClass("sticky");
+
+        }
+        if ($(window).scrollTop() + 90 >= sticky2) {
+            $(leftInfo).addClass("sticky2")
+        } else {
+            $(leftInfo).removeClass("sticky2");
+        }
+    })
+})
---
+++ NewsSiteCoreApp/wwwroot/js/homeIndexScript.js
@@ -0,0 +1,72 @@
+﻿$(function () {
+
+    var headerContainer = $(".header-container");
+    var sticky = $(headerContainer).offset().top;
+    $(window).scroll(function () {
+
+        if ($(window).scrollTop() > sticky) {
+            $(headerContainer).addClass("sticky")
+
+        }
+        else if ($(window).scrollTop() == sticky) {
+            $(headerContainer).removeClass("sticky");
+        }
+
+    })
+    //Slider written here
+    var countOfSliderImage = $(".slider-content .slider-item").length;
+    var margin = 0;
+    var i = 0;
+    setInterval(function () { addMargin() }, 5000);
+
+    $(document).on("click", ".slider-arrow", function () {
+        if ($(this).hasClass("slider-arrow_next")) {
+            if ((countOfSliderImage - 1) == i) {
+                margin = 0
+                i = 0
+            }
+            else {
+                margin -= 620;
+                i++
+            }
+         
+            $(".slider-content").css({
+                marginLeft: '' + margin + '' + 'px'
+
+            })
+        }
+        else if ($(this).hasClass("slider-arrow_prev")) {
+       
+
+            if (i == 0) {
+                margin = (-620) * (countOfSliderImage - 1)
+                i = countOfSliderImage - 1
+            }
+            else {
+                margin += 620;
+                i--
+            }
+       
+            $(".slider-content").css({
+                marginLeft: '' + margin + '' + 'px'
+
+            })
+        }
+    })
+    function addMargin() {
+
+        if ((countOfSliderImage - 1) == i) {
+            margin = 0
+            i = 0
+        }
+        else {
+            margin -= 620;
+            i++
+        }
+       
+        $(".slider-content").css({
+            marginLeft: '' + margin + '' + 'px'
+
+        })
+    }
+})
